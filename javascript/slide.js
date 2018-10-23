0b263814ddc00846649c7f853aafd73a84ef200c
kimthanun <thanunkim168@gmail.com>
2018-10-23 21:14:46 +0700

    web site

---
+++ javascript/slide.js
@@ -0,0 +1,7 @@
+﻿var pic = ["computer1.jpg", "phone.jpeg",];
+var j = 1;
+function change() {
+    document.getElementById("ima").src="image/"+pic[j++];
+    if (j == 2) { j = 0;}
+}
+setInterval(change, 1000);
